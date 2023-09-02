2a00f12ff20ce53877c82a9277e83ee4588f6ca2
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-09-02 09:58:32 -0300

    Add files via upload

---
+++ Java/Nota de Aula #1/Q4/ContaBancaria.java
@@ -0,0 +1,20 @@
+public class ContaBancaria {
+
+    double saldo = 0.0;
+
+    public double ConsultarSaldo() {
+        return saldo;
+    }
+
+    public void Deposito(double quantia) {
+        saldo = saldo + quantia;
+    }
+
+    public void Saque(double quantia) {
+        saldo = saldo - quantia;
+    }
+
+    public void Transferir(double quantia, String destinatario) {
+        saldo = saldo - quantia;
+    }
+}
