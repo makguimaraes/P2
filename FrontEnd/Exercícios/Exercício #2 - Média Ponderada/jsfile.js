ba0ed6be5d6e1ef38c20908f7dbb989ace5a62f4
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-09-02 10:13:09 -0300

    Create jsfile.js

---
+++ FrontEnd/Exercícios/Exercício #2 - Média Ponderada/jsfile.js
@@ -0,0 +1,7 @@
+nota1 = parseFloat(prompt("Informe a primeira nota: "));
+nota2 = parseFloat(prompt("Informe a segunda nota: "));
+nota3 = parseFloat(prompt("Informe a terceira nota: "));
+
+media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10;
+
+alert(`A média do aluno foi ${media}`)
