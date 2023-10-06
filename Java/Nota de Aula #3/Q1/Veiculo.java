63d423896734c53f0a44a6a767fdb97e9a7abcee
makguimaraes <127980915+makguimaraes@users.noreply.github.com>
2023-10-06 15:24:25 -0300

    Nota de Aula #3

---
+++ Java/Nota de Aula #3/Q1/Veiculo.java
@@ -0,0 +1,40 @@
+public class Veiculo {
+
+    protected String marca;
+    protected String modelo;
+    protected int anoFabricacao;
+
+
+    public Veiculo() {
+    }
+
+    public Veiculo(String marca, String modelo, int anoFabricacao) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+}
